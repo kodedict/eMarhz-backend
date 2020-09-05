f0245158ca032659d01853bd16c2fdd23d505171
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-05 11:08:18 +0100

    Role Seeder

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $adminRole                = new Role();
+        $adminRole['name']        = "Administrator";
+        $adminRole['description'] = "Administrator privilege";
+        $adminRole->save();
+
+        $sellerRole                = new Role();
+        $sellerRole['name']        = "Seller/Creator";
+        $sellerRole['description'] = "Seller/Creator privilege";
+        $sellerRole->save();
+
+        $customerRole                = new Role();
+        $customerRole['name']        = "Customer";
+        $customerRole['description'] = "Customer privilege";
+        $customerRole->save();
+    }
+}
