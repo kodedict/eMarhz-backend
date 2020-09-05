cfdca81e1f55825a055ac9666867ed4d629a124c
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-05 20:47:10 +0100

    Order Controller : PASSED

--- app/Http/Controllers/API/customer/orderController.php
+++ app/Http/Controllers/API/customer/orderController.php
@@ -3,13 +3,31 @@
 namespace App\Http\Controllers\API\customer;
 
 use App\Http\Controllers\API\apiController as apiController;
+use App\Order;
 use Illuminate\Http\Request;
 
 class orderController extends apiController
 {
     //
 
-    public function order($id){}
+    public function order($id){
 
-    public function placeOrder(Request $request){}
+        $fetch = Order::query()->where('customerID',$id)->get()->sortByDesc('id')->all();
+
+        if($fetch){return $this->sendResponse($fetch,'success');}
+        else{return $this->sendError('fail','fail');}
+    }
+
+    public function placeOrder(Request $request){
+
+        $post = Order::query()->create([
+            'orderDetail'  => $request['order'],
+            'customerID'   => $request['customer'],
+            'sellerID'     => $request['seller'],
+            'paymentRef'   => $request['payment']
+        ]);
+
+        if($post){return $this->sendResponse('','success');}
+        else{return $this->sendError('fail','fail');}
+    }
 }
