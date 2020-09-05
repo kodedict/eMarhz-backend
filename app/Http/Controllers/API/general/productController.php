810edfe9f7a55e94156141838c8e80bb68d83d6c
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-05 20:14:59 +0100

    General Product Controller : PASSED

--- app/Http/Controllers/API/general/productController.php
+++ app/Http/Controllers/API/general/productController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers\API\general;
 
 use App\Http\Controllers\API\apiController as apiController;
+use App\Product;
 
 class productController extends apiController
 {
     //
 
-    public function getProduct(){}
+    public function getProduct(){
+
+        $fetch = Product::query()->get()->sortByDesc('id')->all();
+
+        if($fetch){return $this->sendResponse($fetch,'success');}
+        else{return $this->sendError('fail','fail');}
+
+    }
 }
