6965d307b1cc5b3a2baeb1dedbb74109a525490e
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-05 10:45:23 +0100

    Order

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
+    protected $fillable =[
+        'orderDetail',
+        'customerID',
+        'sellerID',
+        'price'
+    ];
 }
--- database/migrations/2020_09_05_093441_create_orders_table.php
+++ database/migrations/2020_09_05_093441_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->text('orderDetail');
+            $table->string('customerID','11');
+            $table->string('sellerID');
             $table->timestamps();
         });
     }
