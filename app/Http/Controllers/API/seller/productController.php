4de9679d861fdf0dd6b40f9c2bec59238d732477
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-05 20:08:07 +0100

    Seller Product Controller : PASSED

--- app/Http/Controllers/API/seller/productController.php
+++ app/Http/Controllers/API/seller/productController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\seller;
 
 use App\Http\Controllers\API\apiController as apiController;
+use App\Product;
+use App\Supply;
 use Illuminate\Http\Request;
 
 class productController extends apiController
@@ -10,14 +12,65 @@ class productController extends apiController
     //
 
     //Return page product list
-    public function index(){}
+    public function index(){
+
+        $fetch = Product::query()->get()->sortByDesc('id')->all();
+
+        if($fetch){return $this->sendResponse($fetch,'success');}
+        else{return $this->sendError('fail','fail');}
+    }
 
     //Return store product form
-    public function store(Request $request){}
+    public function store(Request $request){
+
+        $post = Product::query()->create([
+            'name'        => $request['name'],
+            'quantity'    => $request['quantity'],
+            'image'       => $request['image'],
+            'supplierID'  => $request['supplier'],
+            'price'       => $request['price']
+        ]);
+
+        if($post){return $this->sendResponse('','success');}
+        else{return $this->sendError('fail','fail');}
+    }
 
     //Return update product
-    public function update($id){}
+    public function update(Request $request,$id){
+
+        $query = Product::query()->find($id);
+        if(!$query){return $this->sendError('fail','fail');}
+        else{
+            $query['name']       = $request['name'];
+            $query['quantity']   = $request['quantity'];
+            $query['image']      = $request['image'];
+            $query['supplierID'] = $request['supplier'];
+            $query['price']      = $request['name'];
+
+            $update = $query->save();
+
+            if($update){return $this->sendResponse('','success');}
+            else{return $this->sendError('fail','fail');}
+        }
+    }
 
     //Return delete product
-    public function delete($id){}
+    public function delete($id){
+
+        $query = Product::query()->find($id);
+        if(!$query){return $this->sendError('fail','fail');}
+        else{
+            $delete = $query->delete();
+            if($delete){return $this->sendResponse('','success');}
+            else{return $this->sendError('fail','fail');}
+        }
+    }
+
+    //Extra form API to display supplier
+    public function formExtra(){
+        $fetch = Supply::all();
+
+        if($fetch){return $this->sendResponse($fetch,'success');}
+        else{return $this->sendError('fail','fail');}
+    }
 }
