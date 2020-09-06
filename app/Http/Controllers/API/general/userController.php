2bcc544669bb57fb57f568376bdb1c9f4875ae09
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-06 01:13:19 +0100

    Authentication integrated

--- app/Http/Controllers/API/general/userController.php
+++ app/Http/Controllers/API/general/userController.php
@@ -3,15 +3,51 @@
 namespace App\Http\Controllers\API\general;
 
 use App\Http\Controllers\API\apiController as apiController;
+use App\User;
+use App\UserProfile;
 use Illuminate\Http\Request;
 
 class userController extends apiController
 {
     //
 
-    public function login(Request $request){}
+    public function login(Request $request){
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
 
-    public function register(Request $request){}
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return $this->sendResponse($accessToken,'success');
+
+    }
+
+    public function register(Request $request){
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        $validatedData['password'] = bcrypt($request['password']);
+
+        $user = User::query()->create($validatedData);
+
+        UserProfile::query()->create([
+            'userID'      => $user['id'],
+            'billingInfo' => 'nil'
+        ]);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return $this->sendResponse($accessToken,'success');
+    }
 
 //    protected function forgetPW(Request $request){}
 }
