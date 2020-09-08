7a1b71c1edb5d06688ee5d0ec2ec35de07ea5a19
olumayokun olayinka <41644496+kodedict@users.noreply.github.com>
2020-09-08 02:59:44 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("mysql://b193c7673736f4:6f81b673@us-cdbr-east-02.cleardb.co
-m/heroku_013841f035c1fa5?reconnect=true"));
 
-$host =     $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 
 return [
 
@@ -113,8 +107,7 @@ return [
   'prefix' => '',
   'strict' => false,
 ],
-    ],
-    'heroku_mysql_connection' => array(
+'heroku_mysql_connection' => array(
     'driver' => 'mysql',
     'host' => $host,
     'database' => $database,
@@ -124,6 +117,8 @@ return [
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
 ),
+    ],
+    
 
     /*
     |--------------------------------------------------------------------------
